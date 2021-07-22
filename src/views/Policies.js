862e435a7dd2dda8c761478f1cf1a71ef46dbeff
Arvind Rajaraman <arvindrajaraman@users.noreply.github.com>
2021-07-22 13:01:02 -0400

    Skeleton of project complete

---
+++ src/views/Policies.js
@@ -0,0 +1,11 @@
+import './Policies.css';
+
+function Policies() {
+    return (
+        <div className="Policies">
+            Policies page
+        </div>
+    );
+}
+
+export default Policies;
---
+++ src/views/Syllabus.js
@@ -0,0 +1,11 @@
+import './Syllabus.css';
+
+function Syllabus() {
+    return (
+        <div className="Syllabus">
+            Syllabus page
+        </div>
+    );
+}
+
+export default Syllabus;
