8dc037220faca9d1b7cd470075abd9659f12d095
Arvind Rajaraman <arvindrajaraman@users.noreply.github.com>
2021-07-24 16:51:00 -0400

    Finished policies page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {Container, Nav, Navbar} from "react-bootstrap";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, NavLink, Redirect} from "react-router-dom";
 import Policies from "./views/Policies";
 import Syllabus from "./views/Syllabus";
 import Instructors from "./views/Instructors";
@@ -11,17 +11,27 @@ function App() {
             <div className="App">
                 <Navbar className="fixed-top" bg="dark" variant="dark">
                     <Container>
-                        <Navbar.Brand href="/">
-                            Rec Sys in ML
+                        <Navbar.Brand>
+                            <NavLink className="navLink brand" to="/syllabus">Rec Sys in ML</NavLink>
                         </Navbar.Brand>
                         <Navbar.Toggle />
                         <Navbar.Collapse className="justify-content-end">
                             <Nav>
-                                <Nav.Link href="/">
-                                    Syllabus
+                                <Nav.Link>
+                                    <NavLink className="navLink" to="/syllabus" activeClassName="active">Syllabus</NavLink>
                                 </Nav.Link>
-                                <Nav.Link href="/policies">Course Policies</Nav.Link>
-                                <Nav.Link href="/staff">Course Staff</Nav.Link>
+                                <Nav.Link>
+                                    <NavLink className="navLink" to="/policies" activeClassName="active">Policies</NavLink>
+                                </Nav.Link>
+                                <Nav.Link>
+                                    <NavLink className="navLink" to="/staff" activeClassName="active">Staff</NavLink>
+                                </Nav.Link>
+                                <Nav.Link className="navLink" href="https://ml.berkeley.edu" target="_blank" rel="noreferrer">
+                                    ML@B
+                                </Nav.Link>
+                                <Nav.Link className="navLink" href="https://eecs.berkeley.edu" target="_blank" rel="noreferrer">
+                                    EECS
+                                </Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
@@ -29,14 +39,17 @@ function App() {
                 <div className="ghostNavbar" />
 
                 <Switch>
-                    <Route path="/policies">
+                    <Route exact path="/syllabus">
+                        <Syllabus />
+                    </Route>
+                    <Route exact path="/policies">
                         <Policies />
                     </Route>
-                    <Route path="/staff">
+                    <Route exact path="/staff">
                         <Instructors />
                     </Route>
-                    <Route path="/">
-                        <Syllabus />
+                    <Route exact path="/">
+                        <Redirect to="/syllabus" />
                     </Route>
                 </Switch>
             </div>
--- src/views/Syllabus.js
+++ src/views/Syllabus.js
@@ -1,5 +1,6 @@
 import './Syllabus.css';
 import {Button, ButtonGroup, Container, Table} from "react-bootstrap";
+import {Link45deg as LinkIcon} from "react-bootstrap-icons";
 import SyllabusContent from "../content/syllabus.json"
 
 function Syllabus() {
@@ -17,23 +18,23 @@ function Syllabus() {
                             Fall 2021 | 2 Units<br /><br />
                         </td>
                         <td>
-                            <a href="https://ml.berkeley.edu/" target="_blank">
+                            <a href="https://ml.berkeley.edu/" target="_blank" rel="noreferrer">
                                 <img className="mlabLargeLogo" src="/images/sponsors.png" alt="ML@B Logo" />
                             </a>
                         </td>
                     </tr>
                 </table>
                 <ButtonGroup size="sm">
-                    <Button variant="outline-primary" href="https://forms.gle/qjxk7ALqbFCLhTy4A" target="_blank">
+                    <Button variant="outline-primary" href="https://forms.gle/qjxk7ALqbFCLhTy4A" target="_blank" rel="noreferrer">
                         Enrollment Application
                     </Button>
-                    <Button variant="outline-primary" href="https://forms.gle/9EbukMyJEQrB27R69" target="_blank">
+                    <Button variant="outline-primary" href="https://forms.gle/9EbukMyJEQrB27R69" target="_blank" rel="noreferrer">
                         Audit Form
                     </Button>
-                    <Button variant="outline-primary" href="https://piazza.com/class/krgt681dpei5cn" target="_blank">
+                    <Button variant="outline-primary" href="https://piazza.com/class/krgt681dpei5cn" target="_blank" rel="noreferrer">
                         Piazza
                     </Button>
-                    <Button variant="outline-primary" href="https://www.gradescope.com/courses/281648" target="_blank">
+                    <Button variant="outline-primary" href="https://www.gradescope.com/courses/281648" target="_blank" rel="noreferrer">
                         Gradescope
                     </Button>
                 </ButtonGroup>
@@ -48,6 +49,7 @@ function Syllabus() {
                             <th>Date</th>
                             <th>Topic</th>
                             <th>Lecture</th>
+                            <th>Quiz</th>
                             <th>Assignment</th>
                             <th>Readings</th>
                         </tr>
@@ -55,7 +57,6 @@ function Syllabus() {
                     <tbody>
                         {content.map((week, idx) => {
                             const rowspan = (week.readings.length === 0) ? 1 : week.readings.length;
-                            const firstReading = (week.readings.length === 0) ? {} : week.readings[0];
 
                             return [
                                 <tr>
@@ -70,15 +71,59 @@ function Syllabus() {
                                     </td>
                                     <td className="fullRowText" rowspan={rowspan}>
                                         {week.lectureLink && (
-                                            <a href={week.lectureLink} target="_blank">Lecture {idx+1}</a>
+                                            <span>
+                                                Lecture {idx+1}{' '}
+                                                <a href={week.lectureLink} target="_blank" rel="noreferrer">
+                                                    <LinkIcon />
+                                                </a>
+                                            </span>
                                         )}
                                     </td>
-                                    <td rowspan={rowspan}>{week.assignmentName}</td>
-                                    <td>{firstReading.title}</td>
+                                    <td className="fullRowText" rowspan={rowspan}>
+                                        {week.quizLink && (
+                                            <span>
+                                                Quiz {idx+1}{' '}
+                                                <a href={week.quizLink} target="_blank" rel="noreferrer">
+                                                    <LinkIcon />
+                                                </a>
+                                            </span>
+                                        )}
+                                    </td>
+                                    <td rowspan={rowspan}>
+                                        {week.assignment && (
+                                            <span>
+                                                {week.assignment.name}{' '}
+                                                <a href={week.assignment.link} target="_blank" rel="noreferrer">
+                                                    <LinkIcon />
+                                                </a>
+                                            </span>
+                                        )}
+                                    </td>
+                                    <td>
+                                        {week.readings.length > 0 && (
+                                            <span>
+                                                {week.readings[0].title}{' '}
+                                                <a href={week.readings[0].link} target="_blank" rel="noreferrer">
+                                                    <LinkIcon />
+                                                </a>
+                                                <br />
+                                                <span style={{fontWeight: '300'}}>by {week.readings[0].author} ({week.readings[0].year})</span>
+                                            </span>
+                                        )}
+                                    </td>
                                 </tr>,
                                 week.readings.slice(1).map(reading => (
                                     <tr>
-                                        <td>{reading.title}</td>
+                                        <td>
+                                            <span>
+                                                {reading.title}{' '}
+                                                <a href={reading.link} target="_blank" rel="noreferrer">
+                                                    <LinkIcon />
+                                                </a>
+                                                <br />
+                                                <span style={{fontWeight: '300'}}>by {reading.author} ({reading.year})</span>
+                                            </span>
+                                        </td>
                                     </tr>
                                 ))
                             ];
